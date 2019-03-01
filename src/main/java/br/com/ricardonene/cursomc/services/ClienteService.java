56e89194ae8ee84fbef4a88a967adbf44bd4fbaa
Ricardo Nenê <ricardo.nene@gmail.com>
2019-03-01 15:12:50 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/ricardonene/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.ricardonene.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ricardonene.cursomc.domain.Categoria;
+import br.com.ricardonene.cursomc.domain.Cliente;
+import br.com.ricardonene.cursomc.repositories.ClienteRepository;
+import br.com.ricardonene.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		
+		return obj.orElseThrow( () -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName() ) );
+	}
+}
