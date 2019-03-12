e01de6ca3bada729b5180c4364c3f2264bcbfb09
Ricardo Nenê <ricardo.nene@gmail.com>
2019-03-12 09:28:56 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/br/com/ricardonene/cursomc/domain/Cliente.java
+++ src/main/java/br/com/ricardonene/cursomc/domain/Cliente.java
@@ -29,20 +29,20 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
-	@OneToMany(mappedBy="cliente")
+
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
-	
+
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
+
 	@JsonIgnore
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-	
+
 	public Cliente() {
-		
+
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -51,7 +51,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCodigo();
+		this.tipo = tipo == null ? null : tipo.getCodigo();
 	}
 
 	public Integer getId() {
@@ -143,7 +143,4 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
-	
-	
-	
 }
--- src/main/java/br/com/ricardonene/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/ricardonene/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package br.com.ricardonene.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ricardonene.cursomc.domain.Cliente;
+import br.com.ricardonene.cursomc.dto.ClienteDTO;
 import br.com.ricardonene.cursomc.services.ClienteService;
 
 @RestController
@@ -21,5 +30,45 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listaClientes = new ArrayList<>();
+		for (Cliente categoria : list) {
+			ClienteDTO c = new ClienteDTO(categoria);
+			listaClientes.add(c);
+		}
+		return ResponseEntity.ok(listaClientes);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linePerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		
+		Page<Cliente> list = service.findPage(page, linePerPage, orderBy, direction);
+		
+		Page<ClienteDTO> listaClientes = list.map(obj -> new ClienteDTO(obj));
+				
+		return ResponseEntity.ok(listaClientes);
 	}
 }
